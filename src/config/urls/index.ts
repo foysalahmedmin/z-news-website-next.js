ca9a22eab5a568d7b19b9e5da3ce8bc1dfc103c0
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-08-23 10:49:45 +0600

    --

--- src/config/urls/index.ts
+++ src/config/urls/index.ts
@@ -3,7 +3,7 @@ import { ENV } from "../env";
 export const URLS = {
   app: ENV.app_url,
   api: ENV.api_url,
-  user: `${ENV.api_url}/uploads/images/users`,
+  user: `${ENV.api_url}/uploads/users`,
   news: {
     thumbnail: `${ENV.api_url}/uploads/news/thumbnails`,
     image: `${ENV.api_url}/uploads/news/images`,
