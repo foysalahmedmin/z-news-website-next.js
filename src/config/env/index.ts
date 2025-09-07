9ceb84d877642350c18304441ea9ba8c5910361e
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-09-07 13:38:12 +0600

    -

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  app_url: process.env.NEXT_PUBLIC_APP_URL || "https://admin.dainikeidin.com",
-  api_url: process.env.NEXT_PUBLIC_API_URL || "https://test.dainikeidin.com",
+  app_url: process.env.NEXT_PUBLIC_APP_URL || "https://test.dainikeidin.com",
+  api_url: process.env.NEXT_PUBLIC_API_URL || "https://admin.dainikeidin.com",
 };
