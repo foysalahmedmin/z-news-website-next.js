a9fc0cc6db6dcdda52b1d1dbae90677f5005f169
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-09-18 14:53:20 +0600

    Update App Url

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  app_url: process.env.NEXT_PUBLIC_APP_URL || "https://test.dainikeidin.com",
+  app_url: process.env.NEXT_PUBLIC_APP_URL || "https://dainikeidin.com",
   api_url: process.env.NEXT_PUBLIC_API_URL || "https://admin.dainikeidin.com",
 };
